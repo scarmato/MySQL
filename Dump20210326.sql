3558347e3010df927b3bc4cedb183a0f72947ed6
Leonardo Scarmato Jorge de Paula <ljdp@outlook.com>
2021-03-26 16:36:13 -0300

    Create Dump20210326.sql
    Primeiro DUMP

---
+++ Dump20210326.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+CREATE DATABASE cadastro;
+USE cadastro; 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursos`
+--
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+
+CREATE TABLE `cursos` (
+  `idcurso` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `descricao` text,
+  `carga` int(10) unsigned DEFAULT NULL,
+  `totaulas` int(10) unsigned DEFAULT NULL,
+  `ano` year(4) DEFAULT NULL,
+  PRIMARY KEY (`idcurso`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'HTML5','Curso de HTML5',40,37,2014),(2,'Algoritmos','Lógica de Programação',20,15,2014),(3,'Photoshop','Dicas de Photoshop CC',10,8,2014),(4,'PHP','Curso de PHP para iniciantes',40,20,2015),(5,'Java','Introdução à Linguagem Java',40,29,2015),(6,'MySQL','Banco de Dados MySQL',30,15,2016),(7,'Word','Curso completo de Word',40,30,2016);
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gafanhotos`
+--
+
+DROP TABLE IF EXISTS `gafanhotos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gafanhotos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `profissao` varchar(20) NOT NULL DEFAULT 'Desempregado(a)',
+  `nascimento` date DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `peso` decimal(5,2) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `nacionalidade` varchar(20) DEFAULT 'Brasileiro',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gafanhotos`
+--
+
+LOCK TABLES `gafanhotos` WRITE;
+/*!40000 ALTER TABLE `gafanhotos` DISABLE KEYS */;
+INSERT INTO `gafanhotos` VALUES (1,'Godofredo','Desempregado(a)','1984-01-02','M',78.50,1.83,'Portugal'),(2,'Maria','Desempregado(a)','1999-12-30','F',68.50,1.73,'Brasileiro'),(3,'Creuza','Desempregado(a)','1954-11-22','F',74.50,1.63,'Brasileiro'),(4,'Adalgiza','Astrônomo','1944-08-20','F',72.50,1.53,'Irlanda'),(5,'Cláudio','Jogador de futebol','1974-12-30','M',77.50,1.43,'Brasileiro'),(6,'Pedro','Piloto','1999-01-02','M',71.50,1.88,'Brasileiro'),(7,'Janaína','Desempregado(a)','2000-10-12','F',68.50,1.64,'EUA');
+/*!40000 ALTER TABLE `gafanhotos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-26 16:10:06
